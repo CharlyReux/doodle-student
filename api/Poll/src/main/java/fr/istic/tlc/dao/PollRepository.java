f9d0529791aaad442cfc6f94f6a6b5c1fea6dc32
Charly <charlyreux@gmail.com>
2022-05-12 17:54:20 +0200

    [InProgress] added database on start

---
+++ api/Poll/src/main/java/fr/istic/tlc/dao/PollRepository.java
@@ -0,0 +1,15 @@
+package fr.istic.tlc.dao;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import fr.istic.tlc.domain.Poll;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+@ApplicationScoped
+public class PollRepository implements PanacheRepository<Poll>{
+    
+    public Poll findByID(int id){
+        return find("id",id).firstResult();
+    }
+
+}
