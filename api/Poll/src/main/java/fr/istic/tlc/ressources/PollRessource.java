6a65de370f3364ed080104e639906ba53fc7370d
Charly <charlyreux@gmail.com>
2022-05-14 11:44:16 +0200

    added createPoll and updated readme

--- api/Poll/src/main/java/fr/istic/tlc/ressources/PollRessource.java
+++ api/Poll/src/main/java/fr/istic/tlc/ressources/PollRessource.java
@@ -2,11 +2,16 @@ package fr.istic.tlc.ressources;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 import javax.ws.rs.PathParam;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
@@ -31,7 +36,7 @@ public class PollRessource{
         return p;
     }
 
-    @GetMapping("/oui")
+    @GetMapping("/hello")
     public String getPollTest(){
         return "hello world!";
     }
@@ -41,6 +46,13 @@ public class PollRessource{
         List<Poll> polls = pollRepository.findAll(Sort.by("title", Sort.Direction.Ascending)).list();
 		return new ResponseEntity<>(polls, HttpStatus.OK);
     }
+
+    @PostMapping("/polls")
+	@Transactional
+	public ResponseEntity<Poll> createPoll(@Valid @RequestBody Poll poll) {
+		pollRepository.persist(poll);
+		return new ResponseEntity<>(poll, HttpStatus.CREATED);
+	}
 
 
 }
