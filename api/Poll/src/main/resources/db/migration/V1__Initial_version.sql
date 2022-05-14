e2250f46c5c52ed4fb15b529691b1b8609b5e987
Charly <charlyreux@gmail.com>
2022-05-14 10:17:03 +0200

    copied everything from User

---
+++ api/Poll/src/main/resources/db/migration/V1__Initial_version.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le :  mar. 03 nov. 2020 à 18:32
+-- Version du serveur :  8.0.19
+-- Version de PHP :  7.3.11-0ubuntu0.19.10.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `user`
+--
+CREATE DATABASE IF NOT EXISTS `poll` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `poll`;
+
+-- --------------------------------------------------------
+
+
+
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `hibernate_sequence`
+--
+
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES
+(15),
+(15),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1),
+(1);
+ 
+
+
+
+-- --------------------------------------------------------
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `User`
+--
+
+CREATE TABLE `poll` (
+  `id` bigint NOT NULL,
+  `title` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+
+
+--
+-- Index pour la table `User`
+--
+ALTER TABLE `poll`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
