a1f576eae9c38fa87a437bd8d4172cc5bbd8a064
mathomas <matilin.thomas@etudiant.univ-rennes1.fr>
2022-05-23 13:37:50 +0200

    update meal manager

---
+++ api/MealManager/src/main/java/fr/istic/tlc/domain/meal.java
@@ -0,0 +1,54 @@
+package fr.istic.tlc.domain;
+
+import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+/**
+ * Example JPA entity.
+ *
+ * To use it, get access to a JPA EntityManager via injection.
+ *
+ * {@code
+ *     @Inject
+ *     EntityManager em;
+ *
+ *     public void doSomething() {
+ *         MyEntity entity1 = new MyEntity();
+ *         entity1.setField("field-1");
+ *         em.persist(entity1);
+ *
+ *         List<MyEntity> entities = em.createQuery("from MyEntity", MyEntity.class).getResultList();
+ *     }
+ * }
+ */
+@Entity
+@Schema(name="String",description="String")
+public class meal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @NotBlank
+    @Schema(title="comment content")
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
